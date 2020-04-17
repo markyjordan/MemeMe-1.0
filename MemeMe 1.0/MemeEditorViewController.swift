f51676daead332bed0a1ff02f0b5d887a3b470cb
Mark Jordan <markybjordan@gmail.com>
2020-04-16 23:25:33 -0700

    Renamed the root view controller class.
    This included the adding of UIImagePickerControllerDelegate and UINavigationControllerDelegate protocols to the class declaration.

---
+++ MemeMe 1.0/MemeEditorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MemeEditorViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Marky Jordan on 3/18/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+}
+
