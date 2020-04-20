ccac161e0439cf8aa1924b70ba8c881407be6f10
Mark Jordan <markybjordan@gmail.com>
2020-04-19 23:25:59 -0700

    Created a new file with a delegate class for meme text fields.

---
+++ MemeMe 1.0/MemeTextFieldsDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  MemeTextFieldsDelegate.swift
+//  MemeMe 1.0
+//
+//  Created by Marky Jordan on 4/19/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: MemeTextFieldsDelegate: NSObject, UITextFieldDelegate
+
+class MemeTextFieldsDelegate: NSObject, UITextFieldDelegate {
+
+}
